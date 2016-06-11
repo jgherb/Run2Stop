63ce939b60f2d9d6830d571f12e7e61abc0a0872
Flo <flo@Flbbrs-Mac.local>
2016-06-11 19:26:28 +0200

    Initial commit

---
+++ app/src/main/java/de/jhs/run2stop/MainActivity.java
@@ -0,0 +1,219 @@
+package de.jhs.run2stop;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.osmdroid.bonuspack.routing.MapQuestRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polygon;
+import org.osmdroid.views.overlay.Polyline;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.Menu;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocationListener  {
+
+    private MapView         mMapView;
+    private MapController   mMapController;
+
+    LocationManager locationManager;
+
+    double lat;
+    double lng;
+    LocationManager lm;
+    LocationListener ll;
+
+    double latitude, longitude;
+
+    ArrayList<OverlayItem> anotherOverlayItemArray;
+    String[] sources;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        String date = "Aktuelle Uhrzeit: "+ df.format(Calendar.getInstance().getTime());
+        TextView currentTime = (TextView) findViewById(R.id.tV_current_time);
+        currentTime.setText(date);
+
+
+
+        // get GPS manager -> GPS longitude, latitude in onLocationChanged
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        String locationProvider = LocationManager.GPS_PROVIDER;
+
+
+        try {
+            locationManager.requestLocationUpdates(locationProvider, 0, 0, MainActivity.this);
+        }
+        catch(final SecurityException ex) {
+            Toast.makeText(this, "Leider können wir ohne GPS keine Route berechnen", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+        // MapView integration
+        mMapView = (MapView) findViewById(R.id.mapview);
+        mMapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        mMapView.setBuiltInZoomControls(true);
+        mMapView.setMultiTouchControls(true);
+        mMapController = (MapController) mMapView.getController();
+        mMapController.setZoom(13);
+
+        // more at onLocationChanged
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent toCalulator = new Intent(MainActivity.this, Activity2.class);
+                startActivity(toCalulator);
+            }
+        });
+
+
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+
+        double latitude = (double) (location.getLatitude());
+        double longitude = (double) (location.getLongitude());
+
+        // fuer Verbindungen zwischen zwei Verbindungen
+        // ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
+        GeoPoint startPoint = new GeoPoint(longitude, latitude);
+        //waypoints.add(startPoint);
+        //GeoPoint endPoint = new GeoPoint(48.4, -1.9);
+        //waypoints.add(endPoint);
+
+
+        mMapController.setCenter(startPoint);
+
+        // Marker integration
+        Marker startMarker = new Marker(mMapView);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mMapView.getOverlays().add(startMarker);
+
+        //Drawable d = getResources().getDrawable(R.drawable.ic_current_location);
+
+        // aktualisiert Map
+        mMapView.invalidate();
+
+        // set marker and color
+        //d.setColorFilter(getResources().getColor(R.color.currentLoc), PorterDuff.Mode.MULTIPLY);
+        //startMarker.setIcon(getResources().getDrawable(R.drawable.ic_current_location));
+        startMarker.setTitle("Aktuelle Position");
+
+
+        StrictMode.ThreadPolicy pol = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(pol);
+
+        //RoadManager roadManager = new MapQuestRoadManager("DIGIF2BiDrtD1Xbi28SXrkd9Ap2h73Vn");
+        //roadManager.addRequestOption("routeType=pedestrian");
+        //Road road = roadManager.getRoad(waypoints);
+        //Polyline roadOverlay = RoadManager.buildRoadOverlay(road, this);
+        //mMapView.getOverlays().add(roadOverlay);
+
+        // aktualisiert Map
+        mMapView.invalidate();
+
+        TextView currentLoc = (TextView) findViewById(R.id.gps_current_loc);
+        String st_currentLoc = String.valueOf(latitude)+ " , " + String.valueOf(longitude);
+        currentLoc.setText(st_currentLoc);
+
+
+
+        mMapController.setCenter(startPoint);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+
+
+
+}
